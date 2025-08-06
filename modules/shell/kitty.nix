c2b70fd428679b91f2bb4cce061e5b20daf0562e
LeytonHouck <leyton.houck@gmail.com>
2025-08-05 17:42:48 -0700

    Refactor Kitty configuration: streamline font installation and system-wide setup

--- modules/shell/kitty.nix
+++ modules/shell/kitty.nix
@@ -1,28 +1,24 @@
-# ./modules/gui/kitty.nix
+# ./modules/shell/kitty.nix
 { pkgs, ... }:
 
 {
-  # 1. Install a nice font. JetBrains Mono is a great choice.
-  # We use NerdFonts to get extra icons and glyphs.
+  # Install JetBrains Mono Nerd Font
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
-  # 2. Enable and configure the Kitty terminal.
-  # We enable the program and then set its sub-options individually.
-  programs.kitty.enable = true;
+  # Just install Kitty system-wide
+  environment.systemPackages = with pkgs; [
+    kitty
+  ];
 
-    # 3. Use the catppuccin-nix module to apply the theme.
-  programs.kitty.theme = "Macchiato"; # Can be "Latte", "Frappe", "Macchiato", or "Mocha"
-
-    # 4. Set the font and other custom settings.
-  programs.kitty.settings = {
-      font_family = "JetBrainsMono Nerd Font";
-      font_size = 14;
-      # Macchiato-specific background opacity
-      background_opacity = "0.9";
-      # You can add any other kitty setting here, e.g.:
-      # scrollback_lines = 10000;
-      # enable_audio_bell = false;
-    };
+  # Create a default config for all users
+  environment.etc."xdg/kitty/kitty.conf".text = ''
+    font_family JetBrainsMono Nerd Font
+    font_size 14
+    background_opacity 0.9
+    
+    # Catppuccin Macchiato theme
+    include ${pkgs.kitty-themes}/share/kitty-themes/themes/Catppuccin-Macchiato.conf
+  '';
 }
